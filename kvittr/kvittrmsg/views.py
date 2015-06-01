87e0c078b1b0ed615ae6d091be1426adce790603
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 22:07:15 +0200

    Added pagination to list_msgs

--- kvittr/kvittrmsg/views.py
+++ kvittr/kvittrmsg/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from kvittrmsg.models import Kvittrmsg
 
@@ -13,7 +14,17 @@ def list_msgs(request):
 		new_msg.save()
 
 	msgs = Kvittrmsg.objects.all()
-
+	
+	paginator = Paginator(msgs, 5)
+	
+	page = request.GET.get('page')
+	
+	try:
+		msgs = paginator.page(page)
+	except PageNotAnInteger:
+		msgs = paginator.page(1)
+	except EmptyPage:
+		msgs = paginator.page(paginator.num_pages)
 	context = {'msgs': msgs}
 	return render(request, 'kvittrmsg/list_msgs.html', context)
 
