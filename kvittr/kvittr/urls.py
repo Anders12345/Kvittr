41613d4c1f7261bca0074ff80096f0f77de2b926
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 21:56:24 +0200

    Added kvittrmsg urls

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
 	url(r'^$', include('theme.urls')),
 	url(r'^useraccounts/', include('useraccounts.urls')),
+	url(r'^kvittrmsg/', include('kvittrmsg.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
