02fec96be92ba38e575565208db4f0f0782801dd
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 20:48:30 +0200

    Imported authenticate, login, user, redirect and messages and created user_login view

--- kvittr/useraccounts/views.py
+++ kvittr/useraccounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def user_login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('frontpage')
+        else:
+            messages.add_message(request, messages.INFO, "Login failed")
+            return redirect ('frontpage')
+    return render(request, 'useraccounts/login.html', context)
