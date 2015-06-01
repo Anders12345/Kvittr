11333062d1d66f9cb8bdde15788037492aee46db
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 23:01:33 +0200

    Added add_likes url

--- kvittr/kvittrmsg/urls.py
+++ kvittr/kvittrmsg/urls.py
@@ -5,4 +5,5 @@ from kvittrmsg import views
 urlpatterns = patterns('',
 	url(r'^$', views.list_msgs, name='list_msgs'),
 	url(r'^(\d+)/$', views.msg_details, name='msg_details'),
+	url(r'^(\d+)/add_likes/(\d+)$', views.msg_add_likes, name='msg_add_likes'),
 )
