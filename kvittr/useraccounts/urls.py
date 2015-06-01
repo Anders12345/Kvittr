b9f7467403fedcea0c3cf7467278b0bd7e5278b9
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 21:06:27 +0200

    Added logout url

--- kvittr/useraccounts/urls.py
+++ kvittr/useraccounts/urls.py
@@ -3,4 +3,5 @@ from useraccounts import views
 
 urlpatterns = patterns('',
     url(r'^login$', views.user_login, name='user_login'),
+    url(r'^logout$', views.user_logout, name='user_logout'),
 )
