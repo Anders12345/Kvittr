192e01534d197e64491315039bc61c256cc2c0a4
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 21:43:25 +0200

    Created class Kvittrmsg

--- kvittr/kvittrmsg/models.py
+++ kvittr/kvittrmsg/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Kvittrmsg(models.Model):
+	msg = models.TextField()
+	created_datetime = models.DateTimeField()
+	created_by = models.ForeignKey(User, related_name='created_msg')
+	likes = models.PositiveIntegerField(default=0)
+
+	def __unicode__(self):
+		return u'%s' % self.msg
+
+	class Meta:
+		ordering = ['-id']
