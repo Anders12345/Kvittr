ec83468e35b0fada7f243b78dcd0850682a10b59
Anders Skaatan <andersskatan@hotmail.com>
2015-06-01 21:57:25 +0200

    Created list_msgs function

--- kvittr/kvittrmsg/views.py
+++ kvittr/kvittrmsg/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from kvittrmsg.models import Kvittrmsg
+
+def list_msgs(request):
+	if request.method == 'POST':
+		new_msg_text = request.POST.get('new_msg')
+		new_msg = Kvittrmsg()
+		new_msg.msg = new_msg_text
+		new_msg.created_datetime = timezone.now()
+		new_msg.created_by = request.user
+		new_msg.save()
+
+	msgs = Kvittrmsg.objects.all()
+
+	context = {'msgs': msgs}
+	return render(request, 'kvittrmsg/list_msgs.html', context)
+
